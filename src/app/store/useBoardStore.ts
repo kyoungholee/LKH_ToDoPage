83f1b60584e8d97f58d7d4c284ac1879146bc1bd
kyoungholee <dlrudgh1303@gmail.com>
2025-02-16 23:09:34 +0900

    feat: 전역변수에서 boardId의 시작시점과 끝지점을 관리 할 수 있게 구현

--- src/app/store/useBoardStore.ts
+++ src/app/store/useBoardStore.ts
@@ -12,12 +12,38 @@ interface BoardState {
   removeTodoList: (boardId: number, todoId: number) => void;
   modifyTodoList: (boardId: number, todoId: number, newTitle: string) => void;
   reorderBoards: (sourceIndex: number, destinationIndex: number) => void;
+  updateTodoOrder: (
+    boardId: number,
+    startIndex: number,
+    endIndex: number
+  ) => void;
 }
 
 export const useBoardStore = create<BoardState>()(
   persist(
     (set) => ({
       boardList: [],
+
+      updateTodoOrder: (boardId, startIndex, endIndex) => {
+        set((state) => {
+          const board = state.boardList.find(
+            (boardItem) => boardItem.id === boardId
+          );
+          if (!board) return state;
+
+          const newTodoList = [...board.todoList];
+          const [movedItem] = newTodoList.splice(startIndex, 1);
+          newTodoList.splice(endIndex, 0, movedItem);
+
+          return {
+            boardList: state.boardList.map((boardItem) =>
+              boardItem.id === boardId
+                ? { ...boardItem, todoList: newTodoList }
+                : boardItem
+            ),
+          };
+        });
+      },
 
       addBoard: (title) => {
         const newBoard: BoardInfo = {
