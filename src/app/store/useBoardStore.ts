fa61adabdffa2b15726b2f82cc6ca381e461e2a4
kyoungholee <dlrudgh1303@gmail.com>
2025-02-16 11:19:30 +0900

    feat: zustand에서persist를 이용해 로컬스토리로 관리하며, 각 함수들을 전역에서 관리

---
+++ src/app/store/useBoardStore.ts
@@ -0,0 +1,98 @@
+import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
+import { BoardInfo } from '../typescript/board';
+import { Todo } from '../typescript/todo';
+
+interface BoardState {
+  boardList: BoardInfo[];
+  addBoard: (title: string) => void;
+  removeBoard: (id: number) => void;
+  updateBoardTitle: (id: number, newTitle: string) => void;
+  addTodoToBoard: (boardId: number, newTodo: Todo) => void;
+  removeTodoList: (boardId: number, todoId: number) => void;
+  modifyTodoList: (boardId: number, todoId: number, newTitle: string) => void;
+  reorderBoards: (sourceIndex: number, destinationIndex: number) => void;
+}
+
+export const useBoardStore = create<BoardState>()(
+  persist(
+    (set) => ({
+      boardList: [],
+
+      addBoard: (title) => {
+        const newBoard: BoardInfo = {
+          id: Date.now(),
+          title,
+          todoList: [],
+        };
+        set((state) => ({
+          boardList: [...state.boardList, newBoard],
+        }));
+      },
+
+      removeBoard: (id) => {
+        set((state) => ({
+          boardList: state.boardList.filter((board) => board.id !== id),
+        }));
+      },
+
+      updateBoardTitle: (id, newTitle) => {
+        set((state) => ({
+          boardList: state.boardList.map((board) =>
+            board.id === id ? { ...board, title: newTitle } : board
+          ),
+        }));
+      },
+
+      addTodoToBoard: (boardId, newTodo) => {
+        set((state) => ({
+          boardList: state.boardList.map((board) =>
+            board.id === boardId
+              ? { ...board, todoList: [...board.todoList, newTodo] }
+              : board
+          ),
+        }));
+      },
+
+      removeTodoList: (boardId, todoId) => {
+        set((state) => ({
+          boardList: state.boardList.map((board) =>
+            board.id === boardId
+              ? {
+                  ...board,
+                  todoList: board.todoList.filter((todo) => todo.id !== todoId),
+                }
+              : board
+          ),
+        }));
+      },
+
+      modifyTodoList: (boardId, todoId, newTitle) => {
+        set((state) => ({
+          boardList: state.boardList.map((board) =>
+            board.id === boardId
+              ? {
+                  ...board,
+                  todoList: board.todoList.map((item) =>
+                    item.id === todoId ? { ...item, title: newTitle } : item
+                  ),
+                }
+              : board
+          ),
+        }));
+      },
+
+      reorderBoards: (sourceIndex, destinationIndex) => {
+        set((state) => {
+          const reorderedBoards = [...state.boardList];
+          const [movedBoard] = reorderedBoards.splice(sourceIndex, 1);
+          reorderedBoards.splice(destinationIndex, 0, movedBoard);
+          return { boardList: reorderedBoards };
+        });
+      },
+    }),
+    {
+      name: 'board-storage',
+    }
+  )
+);
